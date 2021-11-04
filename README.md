0247c8b6b20b1c262bcbb9a7a4fbdaf098d61881
Alex de Cora <alexinhocora@gmail.com>
2021-11-05 00:25:34 +0100

    part1-exercise1.1(step1)

---
+++ README.md
@@ -0,0 +1,18 @@
+# Full Stack open
+
+This repo is following the material from the course: [Full Stack open 2021](https://fullstackopen.com/en/).
+
+All the exercise are gonna be submitted from this repo following the recommended directories name structure.
+
+```
+part0
+part1
+  courseinfo
+  unicafe
+  anecdotes
+part2
+  phonebook
+  countries
+```
+
+Exercises [submissions](https://studies.cs.helsinki.fi/stats/courses/fullstackopen/submissions)
---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+    <div>
+        <p>
+            {props.parts.part1} {props.exercises.exercises1}
+        </p>
+        <p>
+            {props.parts.part2} {props.exercises.exercises2}
+        </p>
+        <p>
+            {props.parts.part3} {props.exercises.exercises3}
+        </p>
+    </div>
+);
+
+const Total = (props) => <p>Number of exercises {props.total}</p>;
+
+const App = () => {
+    const course = "Half Stack application development";
+    const parts = {
+        part1: "Fundamentals of React",
+        part2: "Using props to pass data",
+        part3: "State of a component",
+    };
+    const exercises = {
+        exercises1: 10,
+        exercises2: 7,
+        exercises3: 14,
+    };
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} exercises={exercises} />
+            <Total
+                total={
+                    exercises.exercises1 +
+                    exercises.exercises2 +
+                    exercises.exercises3
+                }
+            />
+        </div>
+    );
+};
+
+export default App;
