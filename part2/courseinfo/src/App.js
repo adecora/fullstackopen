c813e2f704aeeeea05e936740861c9239a96c65f
Alex de Cora <alexinhocora@gmail.com>
2021-11-05 23:31:01 +0100

    part2-exercise2.5(step10)

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,38 +1,5 @@
 import React from "react";
-
-const Course = ({ course: { name, parts } }) => (
-    <div>
-        <Header course={name} />
-        <Content parts={parts} />
-        <Total exercises={parts.map((part) => part.exercises)} />
-    </div>
-);
-
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Part = ({ part, exercises }) => (
-    <p>
-        {part} {exercises}
-    </p>
-);
-
-const Content = ({ parts }) => (
-    <div>
-        {parts.map((part) => (
-            <Part key={part.id} part={part.name} exercises={part.exercises} />
-        ))}
-    </div>
-);
-
-const Total = ({ exercises }) => {
-    const total = exercises.reduce((total, act) => (total += act), 0);
-
-    return (
-        <p>
-            <strong>total of {total} exercises</strong>
-        </p>
-    );
-};
+import Course from "./components/Course";
 
 const App = () => {
     const courses = [
