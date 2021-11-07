4f308725b8b6bda1c6b2ec440f6bf81cc1de71d1
Alex de Cora <alexinhocora@gmail.com>
2021-11-07 18:13:50 +0100

    part2-exercise2.16(step8)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import personService from "./services/persons";
 
 const Filter = ({ name, onChange }) => (
     <div>
@@ -44,9 +44,9 @@ const App = () => {
     const [filterName, setFilterName] = useState("");
 
     useEffect(() => {
-        axios
-            .get("http://localhost:3001/persons")
-            .then((response) => setPersons(response.data));
+        personService
+            .getAll()
+            .then((initialPersons) => setPersons(initialPersons));
     }, []);
 
     const personsToShow = !filterName
@@ -72,13 +72,11 @@ const App = () => {
                 number: newNumber,
             };
 
-            axios
-                .post("http://localhost:3001/persons", newPerson)
-                .then((response) => {
-                    setPersons(persons.concat(response.data));
-                    setNewName("");
-                    setNewNumber("");
-                });
+            personService.create(newPerson).then((returnedPerson) => {
+                setPersons(persons.concat(returnedPerson));
+                setNewName("");
+                setNewNumber("");
+            });
         }
     };
 
