927401a0842ce274cdc653c856a4dd0925da2580
Alex de Cora <alexinhocora@gmail.com>
2021-11-19 12:34:05 +0100

    part4-exercise4.11(step4)

--- part4/test/blogs-api.test.js
+++ part4/test/blogs-api.test.js
@@ -53,6 +53,21 @@ test('added a valid blog', async () => {
   expect(titles).toContain('freeCodeCamp')
 })
 
+test('blog wihout likes property, default value to 0', async () => {
+  const newBlog = {
+    title: 'The Odin Project',
+    author: 'Erik Trautman',
+    url: 'https://www.theodinproject.com/'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(0)
+})
 
 afterAll(() => {
   mongoose.connection.close()
