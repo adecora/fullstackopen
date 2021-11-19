cd1a0c551dde9a103620e87b971b5380c94294d6
Alex de Cora <alexinhocora@gmail.com>
2021-11-19 11:09:26 +0100

    part4-exercise4.8(step1)

---
+++ part4/test/blogs-api.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeAll(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.listWithBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.listWithBlogs.length)
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- part4/test/list_helper.test.js
+++ part4/test/list_helper.test.js
@@ -1,66 +1,5 @@
 const listHelper = require('../utils/list_helper')
-
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  }
-]
-
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+const helper = require('./test_helper')
 
 
 test('dummy returns one', () => {
@@ -78,13 +17,13 @@ describe('total likes', () => {
   })
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
+    const result = listHelper.totalLikes(helper.listWithOneBlog)
 
     expect(result).toBe(5)
   })
 
   test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes(helper.listWithBlogs)
 
     expect(result).toBe(36)
   })
@@ -98,7 +37,7 @@ describe('favorite blog', () => {
   })
 
   test('when a list has only one blog, this is the favorite one', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog)
+    const result = listHelper.favoriteBlog(helper.listWithOneBlog)
 
     expect(result).toEqual({
       title: 'Go To Statement Considered Harmful',
@@ -108,7 +47,7 @@ describe('favorite blog', () => {
   })
 
   test('of a bigger list is the right one', () => {
-    const result = listHelper.favoriteBlog(blogs)
+    const result = listHelper.favoriteBlog(helper.listWithBlogs)
 
     expect(result).toEqual({
       title: 'Canonical string reduction',
@@ -126,7 +65,7 @@ describe('most blogs', () => {
   })
 
   test('when a list has only one blog, this author is the one with most blogs', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog)
+    const result = listHelper.mostBlogs(helper.listWithOneBlog)
 
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
@@ -135,7 +74,7 @@ describe('most blogs', () => {
   })
 
   test('of a bigger list is the one with most entries', () => {
-    const result = listHelper.mostBlogs(blogs)
+    const result = listHelper.mostBlogs(helper.listWithBlogs)
 
     expect(result).toEqual({
       author: 'Robert C. Martin',
@@ -152,7 +91,7 @@ describe('most likes', () => {
   })
 
   test('when a list has only one blog, this author is the one with most likes', () => {
-    const result = listHelper.mostLikes(listWithOneBlog)
+    const result = listHelper.mostLikes(helper.listWithOneBlog)
 
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
@@ -161,7 +100,7 @@ describe('most likes', () => {
   })
 
   test('of a bigger list is the one with most total likes', () => {
-    const result = listHelper.mostLikes(blogs)
+    const result = listHelper.mostLikes(helper.listWithBlogs)
 
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
