61d5919efd80156d71d3267fb69899849078e873
Alex de Cora <alexinhocora@gmail.com>
2021-11-19 16:45:14 +0100

    part4-exercise4.13(step6)

--- part4/test/blogs-api.test.js
+++ part4/test/blogs-api.test.js
@@ -7,102 +7,127 @@ const helper = require('./test_helper')
 
 beforeAll(async () => {
   await Blog.deleteMany({})
-  const blogObjects = helper.listWithBlogs.map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
+  await Blog.insertMany(helper.listWithBlogs)
+})
+
+describe('when there is initially some blogs saved', () => {
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.listWithBlogs.length)
+  })
+
+  test('all blogs returned have and id property', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach(blog => expect(blog.id).toBeDefined())
+  })
+
 })
 
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('adding a new blog', () => {
+
+  test('succed with valid data', async () => {
+    const newBlog = {
+      title: 'freeCodeCamp',
+      author: 'Quincy Larson',
+      url: 'https://www.freecodecamp.org/',
+      likes: 500
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.listWithBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain('freeCodeCamp')
+  })
+
+  test('succed without likes property, default value 0', async () => {
+    const newBlog = {
+      title: 'The Odin Project',
+      author: 'Erik Trautman',
+      url: 'https://www.theodinproject.com/'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(0)
+  })
+
+  test('fails with status code 400 if missing title', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+
+    const newBlog = {
+      author: 'Quincy Larson',
+      url: 'https://www.freecodecamp.org/',
+      likes: 500
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length)
+  })
+
+  test('fails with status code 400 if missing url', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+
+    const newBlog = {
+      title: 'freeCodeCamp',
+      author: 'Quincy Larson',
+      likes: 500
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length)
+  })
+
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
 
-  expect(response.body).toHaveLength(helper.listWithBlogs.length)
-})
+  test('suecceeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-test('id property is on blogs returned', async () => {
-  const response = await api.get('/api/blogs')
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-  expect(response.body[0].id).toBeDefined()
-})
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
 
-test('added a valid blog', async () => {
-  const newBlog = {
-    title: 'freeCodeCamp',
-    author: 'Quincy Larson',
-    url: 'https://www.freecodecamp.org/',
-    likes: 500
-  }
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.listWithBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain('freeCodeCamp')
-})
-
-test('blog wihout likes property, default value to 0', async () => {
-  const newBlog = {
-    title: 'The Odin Project',
-    author: 'Erik Trautman',
-    url: 'https://www.theodinproject.com/'
-  }
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  expect(response.body.likes).toBe(0)
-})
-
-test('missing title in blog post request', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-
-  const newBlog = {
-    author: 'Quincy Larson',
-    url: 'https://www.freecodecamp.org/',
-    likes: 500
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(blogsAtStart.length)
-})
-
-test('missing url in blog post request', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-
-  const newBlog = {
-    title: 'freeCodeCamp',
-    author: 'Quincy Larson',
-    likes: 500
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(blogsAtStart.length)
 })
 
 afterAll(() => {
