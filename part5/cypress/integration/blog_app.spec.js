ce88aafa46764cb4583d5db3ae90e79c1b29b289
Alex de Cora <alexinhocora@gmail.com>
2021-11-28 18:14:32 +0100

    part5-exercise5.19(step2)

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http:localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Alejandro',
+      username: 'adecora',
+      password: '12345'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -11,5 +17,25 @@ describe('Blog app', function () {
     cy.get('[name="Password"]')
 
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('[name="Username"]').type('adecora')
+      cy.get('[name="Password"]').type('12345')
+      cy.contains('login').click()
+
+      cy.contains('Alejandro logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('[name="Username"]').type('adecora')
+      cy.get('[name="Password"]').type('wrong')
+      cy.contains('login').click()
+
+      cy.contains('wrong username or password')
+      cy.get('.notification').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+
   })
 })
