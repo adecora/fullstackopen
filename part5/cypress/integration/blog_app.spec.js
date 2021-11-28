16e58105e921b5ef396bed31dca301fbd120708b
Alex de Cora <alexinhocora@gmail.com>
2021-11-28 17:54:52 +0100

    part5-exercise5.17(step1)

---
+++ part5/cypress/integration/blog_app.spec.js
@@ -0,0 +1,15 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http:localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is show', function () {
+    cy.contains('log in to application')
+
+    cy.get('[name="Username"]')
+    cy.get('[name="Password"]')
+
+    cy.contains('login')
+  })
+})
