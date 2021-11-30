0ec395a8661af436407e8a1b83d097262e6d8ea5
Alex de Cora <alexinhocora@gmail.com>
2021-11-30 12:36:52 +0100

    part6-exercise6.10(step8)

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,7 +4,7 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 import Anecdote from './Anecdote'
 
 const AnecdoteList = ({ title }) => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
     .sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
