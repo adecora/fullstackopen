655cedc411baa347c390317118c25fe658074f47
Alex de Cora <alexinhocora@gmail.com>
2021-12-01 12:32:32 +0100

    part6-exercise6.19(step1)

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,10 +1,9 @@
 
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
-  if (notification === null) {
+const Notification = (props) => {
+  if (props.notification === null) {
     return null
   }
 
@@ -15,9 +14,14 @@ const Notification = () => {
   }
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => ({
+  notification: state.notification
+})
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
