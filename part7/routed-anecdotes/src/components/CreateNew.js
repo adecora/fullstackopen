31c08ffb5fe59e84b78de88e8c22725707acc427
Alex de Cora <alexinhocora@gmail.com>
2021-12-02 12:08:50 +0100

    part7-exercise7.5(step2)

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -17,6 +17,13 @@ const CreateNew = (props) => {
     })
   }
 
+  const reset = () => {
+    console.log('RESET')
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -33,7 +40,8 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset" onClick={reset}>reset</button>
       </form>
     </div>
   )
