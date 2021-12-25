b349e23d2b40f88b78cacaacd368cad6833f1820
Alex de Cora <alexinhocora@gmail.com>
2021-12-25 22:50:42 +0100

    part9-exercise9.10

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,9 @@ app.get('/api/ping', (_req, res) => {
     console.log('Someone ping the server!');
     res.send('pong');
 });
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
