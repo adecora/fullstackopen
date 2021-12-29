dd0e6bbc9e12c7336e8060f0f18209dab6f40a43
Alex de Cora <alexinhocora@gmail.com>
2021-12-29 11:50:35 +0100

    part9-exercise9.19

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -10,9 +10,49 @@ export enum Gender {
     Female = 'female'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseEntry['code']>
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+interface HospitalEnty extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export type Entry =
+    | HealthCheckEntry
+    | HospitalEnty
+    | OccupationalHealthcareEntry;
 export interface PatientEntry {
     id: string;
     name: string;
--- part9/patientor/backend/src/utils.ts
+++ part9/patientor/backend/src/utils.ts
@@ -60,7 +60,8 @@ const toNewPatientEntry = ({ name, dateOfBirth, ssn, gender, occupation }: Field
         dateOfBirth: parseDate(dateOfBirth),
         ssn: parseSsn(ssn),
         gender: parseGender(gender),
-        occupation: parseString(occupation)
+        occupation: parseString(occupation),
+        entries: []
     };
 
     return newEntry;
